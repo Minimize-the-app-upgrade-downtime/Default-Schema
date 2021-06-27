c072f4f907d623d076a28db46f39da1c3218ea1e
DilshanDilipudara <gunawardhanaudara@gmail.com>
2021-06-27 08:57:12 +0530

    change default schema

--- main.go
+++ main.go
@@ -3,73 +3,88 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
+
 )
 
 //User defines model for storing account details in database
-type db_field struct {
-	
-	// office
-	OfficeCode 		string `json:"officeCode,omitempty"`
-	OfficeName 		string `json:"officeName,omitempty"`
-	City 			string `json:"city,omitempty"`
-	AddressLine1  	string `json:"addressLine1,omitempty"`
-	Address 		string `json:"address,omitempty"`
-	AddressLine2 	string `json:"addressLine2,omitempty"`
-	PostalCode 		string `json:"postalCode,omitempty"`
-	Street 			string `json:"street,omitempty"`
-	Country 		string `json:"country,omitempty"`
+type office struct {
 
-	// employee
-	EmployeeNumber  int    `json:"employeeNumber,omitempty",`
-	FirstName       string `json:"firstName,omitempty",`
-	LastName        string `json:"lastName,omitempty",`
-	Email  			string `json:"email,omitempty"`
-	JobTitle  		string `json:"job,omitempty",`
-	PhoneNumber     string `json:"phonenumber,omitempty",`
+	// office
+	OfficeCode   string `json:"officeCode,omitempty"`
+	OfficeName   string `json:"officeName,omitempty"`
+	City         string `json:"city,omitempty"`
+	AddressLine1 string `json:"addressLine1,omitempty"`
+	AddressLine2 string `json:"addressLine2,omitempty"`
+	PostalCode   string `json:"postalCode,omitempty"`
+	Street       string `json:"street,omitempty"`
+	Country      string `json:"country,omitempty"`
+	PhoneNumber  int    `json:"phonenumber,omitempty"`
 }
 
-func main(){
-	fmt.Println("Default Schema Change UP");
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", changeRequestForUpdatedVersion)
+type employee struct {
+	// employee
+	EmployeeNumber int    `json:"employeeNumber,omitempty"`
+	OfficeCode     string `json:"officeCode,omitempty"`
+	FirstName      string `json:"firstName,omitempty"`
+	LastName       string `json:"lastName,omitempty"`
+	Email          string `json:"email,omitempty"`
+	JobTitle       string `json:"job,omitempty"`
 	
+}
+
+func main() {
+	fmt.Println("Default Schema Change up")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", changeRequestForUpdatedVersion)
 	http.ListenAndServe(":50002", mux)
 }
 
-func changeRequestForUpdatedVersion(w http.ResponseWriter, r *http.Request){
+func changeRequestForUpdatedVersion(w http.ResponseWriter, r *http.Request) {
 	log.Println("POST request received from localhost:50000")
-	db := db_field{} //initialize 
 
-	// log.Println(r)
-	//Parse json request body and use it to set fields on db
-	err := json.NewDecoder(r.Body).Decode(&db)
-	
-	if err != nil{
-		panic(err)
+	if r.URL.Path == "/addFormOffice" {
+		dbOffice := office{} //initialize
+		
+		err := json.NewDecoder(r.Body).Decode(&dbOffice)
+		fmt.Println(dbOffice)
+		if err != nil {
+			panic(err)
+		}
+		dbJson, err := json.Marshal(dbOffice)
+		if err != nil {
+			panic(err)
+		}
+		responseBody := bytes.NewBuffer(dbJson)
+		url := "http://localhost:3000" + r.URL.String()
+		resp, err := http.Post(url, "application/json", responseBody)
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+		}
+		defer resp.Body.Close()
+
 	}
-	log.Println("Apply Default Shcema for POST Request")
-	
-	//Marshal or convert user object back to json and write to response 
-	dbJson, err := json.Marshal(db)
-	if err != nil{
-		panic(err)
+
+	if r.URL.Path == "/addFormEmployee" {
+		dbEmp := employee{} // initailize
+		err := json.NewDecoder(r.Body).Decode(&dbEmp)
+		fmt.Println(dbEmp)
+
+		if err != nil {
+			panic(err)
+		}
+		dbJson, err := json.Marshal(dbEmp)
+		if err != nil {
+			panic(err)
+		}
+		responseBody := bytes.NewBuffer(dbJson)
+		url := "http://localhost:3000" + r.URL.String()
+		resp, err := http.Post(url, "application/json", responseBody)
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+		}
+		defer resp.Body.Close()
 	}
-	
-	responseBody := bytes.NewBuffer(dbJson)
-
-	// create a post request
-	url := "http://localhost:3000" + r.URL.String()
-	//log.Println(url)
-	resp, err := http.Post(url, "application/json", responseBody)
-
-	if err != nil {
-		log.Fatalf("An Error Occured %v", err)
-	}
-	defer resp.Body.Close()
-
-	
 }
